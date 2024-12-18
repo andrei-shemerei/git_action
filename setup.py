297ad88e9df62340134f59c2cbb5adcc755a49cb
andrei-shemerei <andrejsemerej537@gmail.com>
2024-12-18 12:04:41 +0300

    DS-4.1 Produstion
    Changes:
    
    - Up package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="",
-    version="0.0.1",
+    version="0.0.2",
     description="A package for feature extraction, hyperparameter optimization, and validation for item sales data.",
     packages=find_packages(),
     install_requires=[
--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,4 +1,4 @@
 from .etl_process import ETL
 from .feature_extraction import main_pipeline
 from .validation_schema import Validator
-__version__ = '0.1.0'
+__version__ = '0.0.2'
