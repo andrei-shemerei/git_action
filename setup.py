d426b2f6964e974df8835961268363698455c50f
unknown <andrejsemerej537@gmail.com>
2024-12-29 01:58:00 +0300

    Rebilt PYPI 5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="feature_sales_prediction_engine",
-    version="0.0.7",
+    version="0.0.8",
     description="A package for feature sales prediction",
     long_description_content_type="text/markdown",
     packages=["feature_sales_prediction_engine"],
--- src/feature_sales_prediction_engine/__init__.py
+++ src/feature_sales_prediction_engine/__init__.py
@@ -4,7 +4,7 @@ from .etl_process import ETL
 from .feature_extraction import main_pipeline
 from .validation_schema import Validator
 
-__version__ = '0.0.7'
+__version__ = '0.0.8'
 
 
 # Убедимся, что все глобальные переменные инициализируются автоматически
