c94011b1eca535dc91e4c365be533051728e2cf5
unknown <andrejsemerej537@gmail.com>
2024-12-28 23:39:02 +0300

    Rebilt PYPI 1

--- src/feature_sales_prediction_engine/__init__.py
+++ src/feature_sales_prediction_engine/__init__.py
@@ -1,4 +1,29 @@
+from . import global_var
+from .utils import config_reader
 from .etl_process import ETL
 from .feature_extraction import main_pipeline
 from .validation_schema import Validator
-__version__ = '0.0.2'
+
+__version__ = '0.0.4'
+
+
+# Убедимся, что все глобальные переменные инициализируются автоматически
+__all__ = [
+    "global_var",
+    "etl_process",
+    "feature_extraction",
+    "hyperopt",
+    "validation_schema",
+    "config_reader",
+]
+
+def init():
+    """
+    Функция инициализации глобальных переменных.
+    """
+    # Проверка на инициализацию переменных
+    if not global_var.SHOP_DUPLICATE_SET or not global_var.CORRECT_CITY_NAME:
+        raise ValueError("Глобальные переменные не были загружены корректно!")
+
+
+init()
