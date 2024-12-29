bdcf61e2edfec3756570763c2c0ea7b352d14dc4
unknown <andrejsemerej537@gmail.com>
2024-12-29 16:19:50 +0300

    Update 9

--- src/feature_sales_prediction_engine/__init__.py
+++ src/feature_sales_prediction_engine/__init__.py
@@ -4,7 +4,7 @@ from .etl_process import ETL
 from .feature_extraction import main_pipeline
 from .validation_schema import Validator
 
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 
 
 # Убедимся, что все глобальные переменные инициализируются автоматически
